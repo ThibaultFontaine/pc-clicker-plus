dba19bd0421c8f19565ad656b64853d0a88fa852
Thibault Fontaine <thibault.fontaine@protonmail.com>
2024-11-25 12:08:25 +0100

    Add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { VBtn } from 'vuetify/lib/components/index.mjs';
 </script>
 
 <template>
   <header>
+    <VBtn color="primary">Primary</VBtn>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
